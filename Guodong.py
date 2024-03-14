47b2d6af21b850c6dedcba6a8816b102cf905a3f
kmarandy <kmarandy@terpmail.umd.edu>
2024-03-14 10:41:52 -0400

    keons changes

--- Guodong.py
+++ Guodong.py
@@ -1,5 +1,5 @@
 def GroupPrint():
     print("hello world")# Allen liu
-print("goodbye") # Tim hwang
-print("hi") #Keon Marandy
-print("have a good break") # Jackson Brunner
+    print("goodbye") # Tim hwang
+    print("hi") #Keon Marandy
+    print("have a good break") # Jackson Brunner
