a8d7c42d53133b7b3977a7f26026ff0f586619f7
kmarandy <kmarandy@terpmail.umd.edu>
2024-05-11 21:41:33 -0400

    Added some unit tests

---
+++ unitTests.py
@@ -0,0 +1,16 @@
+from workingCopy import Song, import_songs_from_csv
+import pytest
+
+def test_project():
+    song = Song("Title", "Artist", "Genre", 2024, 0.8, 0.7, 90)
+    assert song.title == "Title"
+    assert song.artist == "Artist"
+    assert song.top_genre == "Genre"
+    assert song.year == 2024
+    assert song.energy == 0.8
+    assert song.danceability == 0.7
+    assert song.popularity == 90
+    
+def test_import_songs_from_csv():
+    songs = import_songs_from_csv('Top100MostStreamed.csv')  #checks that the right amount of songs are being imported from the csv
+    assert len(songs) == 100
